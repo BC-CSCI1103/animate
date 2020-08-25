67183b52626c659127c0e5e994f571b219ac8c20
Robert Muller <robert.muller2@gmail.com>
2020-08-25 11:53:04 -0400

    adding circle

---
+++ sample/circle/src/bin/main.ml
@@ -0,0 +1,28 @@
+(* file: setting.ml
+ * author: Bob Muller
+ *
+ * A simple animation example, a setting sun over water.
+ *
+ * To run it:
+ * > make
+ * > ./go
+ *
+*)
+type model = int
+
+let view _ = Image.circle 300 (Color.random ())
+
+let update model = model + 1
+
+let finished model = model > 20
+
+let _ =
+  Animate.start 0
+    ~name: "Testing"
+		~width: 600
+		~height: 600
+		~view: view
+		~rate: 1.0
+		~onTick: update
+		~stopWhen: finished
+		~viewLast: view
